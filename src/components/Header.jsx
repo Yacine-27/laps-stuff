356e8e83e56480226449298d1efe5bbf6812224d
yacine <yacine@Xubuntu.myguest.virtualbox.org>
2025-04-22 16:48:29 +0200

    added "_redirects" file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
       className="flex flex-col text-center items-center justify-center gap-2
                 pt-25 pb-15"
     >
-      <h1 className="text-3xl font-extrabold">Tech Deals Made Simple</h1>
+      <h1 className="text-3xl font-extrabold">Tech Deals Made Simple!</h1>
       <h3 className="text-xl font-semibold my-3 text-slate-300">
         Shop the Best Laptops & Phones Today!
       </h3>
